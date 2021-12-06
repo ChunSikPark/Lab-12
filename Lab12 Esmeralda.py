69d5511d6e9c142594ee2785ebc2e86715df9f04
esmeg1032 <95658370+esmeg1032@users.noreply.github.com>
2021-12-06 16:48:14 -0600

    Update Lab12 Esmeralda.py

--- Lab12 Esmeralda.py
+++ Lab12 Esmeralda.py
@@ -18,4 +18,26 @@ Created on Mon Dec  6 11:30:29 2021
     # Date:         06 12 2021
     # 
     
-    
+    from tkinter import *
+  
+# import messagebox from tkinter module
+import tkinter.messagebox
+  
+# create a tkinter root window
+root = tkinter.Tk()
+  
+# root window title and dimension
+root.title("When you press a button the message will pop up")
+root.geometry('500x300')
+  
+# Create a messagebox showinfo
+  
+def onClick():
+    tkinter.messagebox.showinfo("Welcome to GFG.",  "Hi I'm your message")
+  
+# Create a Button
+button = Button(root, text="Click Me", command=onClick, height=5, width=10)
+  
+# Set the position of button on the top of window.
+button.pack(side='bottom')
+root.mainloop()
