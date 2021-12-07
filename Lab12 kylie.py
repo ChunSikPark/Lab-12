d2e7939cedaf442f59e17b899476a0ed65020caa
Kylie Thomas <95657338+kyrhea@users.noreply.github.com>
2021-12-07 12:18:43 -0600

    Update Lab12 kylie.py
    What I have so far for the pig drawing...not done yet

--- Lab12 kylie.py
+++ Lab12 kylie.py
@@ -18,4 +18,76 @@ Created on Mon Dec  6 11:30:29 2021
     # Date:         06 12 2021
     # 
     
-    
+import turtle as t
+
+t.speed(100)
+t.pensize(8)
+t.penup()
+t.goto(0,-150)
+t.pendown()
+
+#head
+t.pencolor("pink")
+t.fillcolor("pink")
+t.begin_fill()
+for i in range(36):
+    t.forward(30)
+    t.left(10)
+t.end_fill()
+
+#eye1
+t.pencolor("black")
+t.penup()
+t.goto(-80,0)
+t.pendown()
+t.fillcolor("black")
+t.begin_fill()
+for i in range(180):
+    t.forward(1)
+    t.left(2)
+t.end_fill()
+
+#eye2
+t.penup()
+t.goto(110,0)
+t.pendown()
+t.begin_fill()
+for i in range(180):
+    t.forward(1)
+    t.left(2)
+t.end_fill()
+
+#snoot
+t.pencolor('#c9759b')
+t.penup()
+t.goto(-30,-50)
+t.pendown()
+t.fillcolor('#c9759b')
+t.begin_fill()
+t.seth(-45)
+for i in range(2):
+    t.circle(60,90)
+    t.circle(60//2,90)
+t.end_fill()
+
+#nostril1
+t.pencolor("#994b6e")
+t.penup()
+t.goto(-30,-40)
+t.pendown()
+t.fillcolor("#994b6e")
+t.begin_fill()
+for i in range(15):
+    t.circle(20,40)
+t.end_fill()
+
+#nostril2
+t.pencolor("#994b6e")
+t.penup()
+t.goto(35,-7)
+t.pendown()
+t.fillcolor("#994b6e")
+t.begin_fill()
+for i in range(15):
+    t.circle(20,40)
+t.end_fill()
